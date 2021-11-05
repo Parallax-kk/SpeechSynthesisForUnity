502e9439c4dfa72cd6c8db9fbe6eb9ad33d8500e
Parallax_kk <kuronekoya.mail@gmail.com>
2021-11-05 11:48:02 +0900

    Initial commit

---
+++ Assets/Sample/Sample.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sample : MonoBehaviour
+{
+    private void Start()
+    {
+        SpeechSynthesis.Speak("これはサンプルボイスです。");
+    }
+}
